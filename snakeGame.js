5ccd61a5f81446771e15ae3a469b9b07b9800c5e
CodyCork <codycork.sfg@outlook.com>
2022-06-29 01:14:45 -0500

    update

--- snakeGame.js
+++ snakeGame.js
@@ -1,30 +1,29 @@
+//canvas
+const canvas = document.getElementById('gameBoard')
+const ctx = canvas.getContext('2d')
 
-var canvas = document.getElementById('gameBoard')
-var ctx = canvas.getContext('2d')
 // snake speed
 let snakeSpeed = 7
+
 //canvas size
 let tileCount = 30
-let tileSize = canvas.width / tileCount - 2;
+let tileSize = canvas.width / tileCount;
 
+//snake head and body
 let headX = 15
 let headY = 15
+const snakeParts = []
+let tailLength = 1
 
 //snake game loop
 const drawGame = () => {
-    clearScreen()
     drawSnake()
     setTimeout(drawGame, 1000 / snakeSpeed)
 }
-
-const clearScreen = () => {
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-}
-
+//Snake on canvas
 const drawSnake = () => {
     ctx.fillStyle = 'red'
-    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
+    ctx.fillRect = (15, 15, tileSize, tileSize )
 }
 
 drawGame()
