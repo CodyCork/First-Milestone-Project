f0caa9ec56f8e228c66c1751662c1484e5ef0460
CodyCork <codycork.sfg@outlook.com>
2022-07-02 01:56:09 -0500

    deleted files and debugged

--- snakeGame.js
+++ snakeGame.js
@@ -1,13 +1,18 @@
 import { keyDown, xInputDirection, yInputDirection } from "./input.js";
-import { drawApple } from "./snakeFood.js";
 import { clearTile, drawSnake } from "./snake.js";
-import { drawSnakeScore, gameOver } from "./snakeScore.js";
+// import { drawSnakeScore, gameOver } from "./snakeScore.js";
 //canvas
 export const canvas = document.getElementById("gameBoard");
 export const ctx = canvas.getContext("2d");
 
 // snake speed
 let snakeSpeed = 7;
+
+let snakeScore = 0;
+
+//snakeFood
+export let appleX = 7;
+export let appleY = 7;
 
 export let inputsXVelocity = 0;
 export let inputsYVelocity = 0;
@@ -16,7 +21,7 @@ export let xVelocity = 0;
 export let yVelocity = 0;
 
 //canvas size
-let tileCount = 30;
+export let tileCount = 30;
 export let tileWidth = canvas.width / tileCount;
 export let tileHeight = canvas.height / tileCount;
 
@@ -33,11 +38,54 @@ export let headY = 15;
 export const snakeParts = [];
 export let tailLength = 2;
 
+export const drawSnakeScore = () => {
+  ctx.fillStyle = "black";
+  ctx.font = "9px Verdana";
+  ctx.fillText("Score " + snakeScore, canvas.width - 300, 148);
+};
+
 const snakeDirection = () => {
   headX += xInputDirection;
   headY += yInputDirection;
 };
 
+const drawApple = () => {
+  ctx.fillStyle = "red";
+  ctx.fillRect(
+    appleX * tileWidth - tileWidth / 2,
+    appleY * tileHeight - tileHeight / 2,
+    tileWidth,
+    tileHeight
+  );
+};
+
+function gameOver() {
+  let gameOver = false;
+
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  //walls
+  if (headX < 0) {
+    gameOver = true;
+  } else if (headX === tileCount) {
+    gameOver = true;
+  } else if (headY < 0) {
+    gameOver = true;
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
+    }
+  }
+  return gameOver;
+}
 //snake game loop
 const drawGame = () => {
   xVelocity = inputsXVelocity;
@@ -48,15 +96,22 @@ const drawGame = () => {
     return;
   }
 
+  if (appleX === headX && appleY == headY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    snakeScore++;
+    //   gulpSound.play();
+  }
+
   clearTile();
 
   drawSnake();
   drawApple();
-
+  //   checkAppleCollision();
   drawSnakeScore();
 
   snakeDirection();
-  gameOver();
 
   setTimeout(drawGame, 1000 / snakeSpeed);
 };
